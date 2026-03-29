8115ff67bec85f4bf215a33cea924619e9a3a024
Alex <sashazjukov@gmail.com>
2026-03-29 17:51:13 +0300

    aerial zt

--- lua/plugins/aerial.lua
+++ lua/plugins/aerial.lua
@@ -1,23 +1,23 @@
 return {
-  "stevearc/aerial.nvim",
-  opts = function()
-    local opts = {
-      attach_mode = "global",
-      show_guides = true,
-      filter_kind = false,
-      open_automatic = false,
-      autojump = true,
-      post_jump_cmd = "normal! zz",
-    }
+    "stevearc/aerial.nvim",
+    opts = function()
+        local opts = {
+            attach_mode = "global",
+            show_guides = true,
+            filter_kind = false,
+            open_automatic = false,
+            autojump = true,
+            post_jump_cmd = "normal! zt",
+        }
 
-    return opts
-  end,
+        return opts
+    end,
 
-  keys = {
-    { "<leader>o", "<cmd>AerialToggle<CR>", desc = "Toggle Aerial outline" },
-  },
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter",
-    "nvim-tree/nvim-web-devicons",
-  },
+    keys = {
+        { "<leader>o", "<cmd>AerialToggle<CR>", desc = "Toggle Aerial outline" },
+    },
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter",
+        "nvim-tree/nvim-web-devicons",
+    },
 }
