41de7f5c77f68ee689c373c11333be9457de52e1
Alex <sashazjukov@gmail.com>
2026-05-08 19:06:36 +0300

    rainbow-delimiters for sql

---
+++ after/queries/sql/rainbow-delimiters.scm
@@ -0,0 +1,19 @@
+(parenthesized_expression
+   "(" @delimiter
+   ")" @delimiter @sentinel) @container
+
+; (call_expression
+;    "(" @delimiter
+;    ")" @delimiter @sentinel) @container
+;
+; (if_expression
+;   "(" @delimiter
+;   ")" @delimiter @sentinel) @container
+;
+; (array_type
+;    "[" @delimiter
+;    "]" @delimiter @sentinel) @container
+;
+; (index_expression
+;    "[" @delimiter
+;    "]" @delimiter @sentinel) @container
