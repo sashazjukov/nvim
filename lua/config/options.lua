3cd68ba9df0f7744df22576ff848ef08bb045835
Alex <sashazjukov@gmail.com>
2026-05-08 18:57:48 +0300

    options file

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,31 +5,31 @@ vim.opt.relativenumber = false
 
 --=[ Syntax from sytax folder ]=--
 vim.filetype.add({
-    extension = {
-        dbout = "dbout",
-        log = "log",
-    },
+  extension = {
+    dbout = "dbout",
+    log = "log",
+  },
 })
 
 function CopyFilePathToClipboard()
-    local filePath = vim.fn.expand("%")
-    vim.fn.setreg("+", filePath)
+  local filePath = vim.fn.expand("%")
+  vim.fn.setreg("+", filePath)
 end
 
 vim.opt.expandtab = true -- Use tabs instead of spaces
-vim.opt.tabstop = 4      -- Number of spaces a <Tab> counts for
-vim.opt.shiftwidth = 4   -- Number of spaces for each indent level
+vim.opt.tabstop = 4 -- Number of spaces a <Tab> counts for
+vim.opt.shiftwidth = 4 -- Number of spaces for each indent level
 
 -- vim.opt.scrolloff = 0
 --vim.opt.mouse = "n"
 
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = "powerbuilder",
-    callback = function()
-        vim.opt.expandtab = true -- Use tabs instead of spaces
-        vim.opt.tabstop = 2      -- Number of spaces a <Tab> counts for
-        vim.opt.shiftwidth = 2   -- Number of spaces for each indent level
-    end,
+  pattern = "powerbuilder",
+  callback = function()
+    vim.opt.expandtab = true -- Use tabs instead of spaces
+    vim.opt.tabstop = 2 -- Number of spaces a <Tab> counts for
+    vim.opt.shiftwidth = 2 -- Number of spaces for each indent level
+  end,
 })
 
 -- vim.g.sql_type_default = "msql"
@@ -47,6 +47,7 @@ vim.g.ftplugin_sql_omni_key_left = ""
 -- vim.api.nvim_set_hl(0, "@string",   { fg = "#bb8fce" }) -- lighter violet for strings
 
 vim.opt_local.list = false -- Enable list only for sensitive filetypes
+
 -- vim.api.nvim_create_autocmd("FileType", {
 --   pattern = { "python", "yaml", "make" }, -- add more if needed
 --   callback = function()
@@ -58,13 +59,18 @@ vim.opt_local.list = false -- Enable list only for sensitive filetypes
 --     }
 --   end,
 -- })
---
--- Disable list for all other filetypes
-vim.api.nvim_create_autocmd("FileType", {
-    pattern = "*",
-    callback = function()
-        -- if not vim.tbl_contains({ "python", "yaml", "make" }, vim.bo.filetype) then
-        vim.opt_local.list = false
-        -- end
-    end,
-})
+
+-- -- Disable list for all other filetypes
+-- vim.api.nvim_create_autocmd("FileType", {
+--   pattern = "*",
+--   callback = function()
+--     -- if not vim.tbl_contains({ "python", "yaml", "make" }, vim.bo.filetype) then
+--     vim.opt_local.list = true
+--     -- end
+--     --  vim.opt_local.listchars = {
+--     --      tab = "",    -- Tabs
+--     --      trail = "~", -- Trailing spaces
+--     --      space = "",  -- Spaces
+--     --  }
+--   end,
+-- })
