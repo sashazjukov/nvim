d2a2d615c46ac97e98b49443499db6d1fa2b1ba4
Alex <sashazjukov@gmail.com>
2026-06-06 12:17:30 +0300

    Update plugin dependencies and add new extras
    - Update all plugin commits in lazy-lock.json
    - Add cmake, markdown, rust, mini-snippets, mini-surround,
      mini-diff, mini-hipatterns extras to lazyvim.json
    - Add XML filetype detection
    - Add devicons config stub for powerbuilder filetypes

---
+++ lua/plugins/devicons.lua
@@ -0,0 +1,55 @@
+return {}
+
+-- require('mini.icons').setup({
+--     extension = {
+--         srw = { glyph = '󰕓', hl = 'MiniIconsGrey' },
+--     },
+-- })
+
+-- return {
+--     "nvim-tree/nvim-web-devicons",
+--     priority = 1000,
+--     config = function()
+--         require("nvim-web-devicons").set_icon_by_filetype({ srw = "srw" })
+--         require("nvim-web-devicons").set_icon_by_filetype({ sru = "sln" })
+--         require("nvim-web-devicons").set_icon_by_filetype({ sln = "srd" })
+--         require("nvim-web-devicons").setup({
+--             overrides = {
+--                 srw = {
+--                     icon = "󰕓",
+--                     name = "Srw",
+--                 },
+--             },
+--         })
+--
+--         require('nvim-web-devicons').set_icon_by_filetype({
+--             srw = {
+--                 icon = '󰟓',
+--                 color = '#3ca7df',
+--                 name = 'Srw',
+--             },
+--         })
+--     end,
+-- }
+
+
+
+-- return {
+--     "nvim-tree/nvim-web-devicons",
+--     priority = 1000,
+--     config = function()
+--         require('nvim-web-devicons').set_icon_by_filetype({
+--             srw = {
+--                 icon = '󰟓',
+--                 color = '#3ca7df',
+--                 name = 'Srw',
+--             },
+--         })
+--
+--         require("nvim-web-devicons").set_icon_by_filetype({ srw = "srw" })
+--     end,
+--     -- config = function()
+--     --     require("nvim-web-devicons").set_icon({ srw = { icon = "", color = "#89E051", cterm_color = "113", name = "Srw" }, })
+--     --     require("nvim-web-devicons").set_icon_by_filetype({ srw = "srw" })
+--     -- end,
+-- }
