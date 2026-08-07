d2af57a0c2929d42fe4a7e674bc8517ceecdee93
Alex <sashazjukov@gmail.com>
2026-08-07 18:08:08 +0300

    feat: exclude repeat/for from lua rainbow-blocks query to preserve @keyword.repeat colors

---
+++ queries/lua/rainbow-blocks.scm
@@ -0,0 +1,70 @@
+;;; Note: Some patterns are commented out because currently Neovim can capture
+;;; only one node at a time.  Once it becomes possible to capture multiple
+;;; nodes they should be commented back in.
+
+
+; (function_declaration
+;   "function" @delimiter
+;   "end" @delimiter) @container
+;
+; (function_definition
+;   "function" @delimiter
+;   "end" @delimiter) @container
+
+(if_statement
+  "if" @delimiter
+  "then" @delimiter
+  (elseif_statement
+    "elseif" @delimiter
+    "then" @delimiter)*
+  (else_statement
+    "else" @delimiter)?
+  "end" @delimiter) @container
+
+; (while_statement
+;   "while" @delimiter
+;   "do" @delimiter
+;   "end" @delimiter) @container
+
+;; Excluded: these keywords get colored as delimiters, overriding @keyword.repeat
+;; (repeat_statement
+;;   "repeat" @delimiter
+;;   "until" @delimiter) @container
+;;
+;; (for_statement
+;;   "for" @delimiter
+;;   (for_generic_clause
+;;     "in" @delimiter)?
+;;   "do" @delimiter
+;;   "end" @delimiter) @container
+
+; (do_statement
+;   "do" @delimiter
+;   "end" @delimiter) @container
+
+
+;;; Copied over from rainbow-parens
+
+(arguments
+  "(" @delimiter
+  ")" @delimiter) @container
+
+(parameters
+  "(" @delimiter
+  ")" @delimiter) @container
+
+(parenthesized_expression
+  "(" @delimiter
+  ")" @delimiter) @container
+
+(table_constructor
+  "{" @delimiter
+  "}" @delimiter) @container
+
+(bracket_index_expression
+  "[" @delimiter
+  "]" @delimiter) @container
+
+(field
+  "[" @delimiter
+  "]" @delimiter) @container
