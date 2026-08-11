6d1d0fb4d667acc5e22379f19b37e537e307ee1c
Alex <sashazjukov@gmail.com>
2026-08-11 18:49:31 +0300

    feat(aerial): show symbols in diff windows, rework keymaps, fix icon highlights

--- lua/plugins/aerial.lua
+++ lua/plugins/aerial.lua
@@ -9,14 +9,16 @@ return {
       autojump = true,
       post_jump_cmd = "normal! zt",
       disable_max_lines = 20000,
+      diff_windows = true,
     }
 
     local hl = vim.api.nvim_set_hl
     local visual_bg = vim.api.nvim_get_hl(0, { name = "Visual", link = false }).bg
     hl(0, "AerialLine", { bg = visual_bg, bold = true })
-    hl(0, "AerialFunctionIcon", { fg = "#ee99ee" })
+    hl(0, "AerialFunctionIcon", { fg = "#aa99cc" })
     hl(0, "AerialFunction", { fg = "#aa99cc" })
     hl(0, "AerialEvent", { fg = "#eebbee" })
+    hl(0, "AerialEventIcon", { fg = "#eebbee" })
     hl(0, "AerialClass", { fg = "#00bbbb" })
     hl(0, "AerialFile", { fg = "#33aa99" })
     hl(0, "AerialFileIcon", { fg = "#33aa99" })
@@ -34,7 +36,14 @@ return {
   end,
 
   keys = {
-    { "<leader>o", "<cmd>AerialToggle<CR>", desc = "Toggle Aerial outline" },
+    { "<leader>oo", "<cmd>AerialToggle<CR>", desc = "Toggle Aerial outline" },
+    {
+      "<leader>os",
+      function()
+        require("aerial").snacks_picker()
+      end,
+      desc = "Aerial outline picker",
+    },
   },
   dependencies = {
     "nvim-treesitter/nvim-treesitter",
